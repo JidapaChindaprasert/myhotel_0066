140df5733dc6093321b44df927c3ce2de48417a6
JidapaChindaprasert <164831889+JidapaChindaprasert@users.noreply.github.com>
2026-02-03 15:24:00 +0700

    add file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Connect to server and select database.
+define('servername', 'localhost:3307');
+define('username', 'root');
+define('password', '');
+define('dbname', 'myhotel_0066');
+$objCon = mysqli_connect(servername, username,password,dbname);
+?>
---
+++ testconnect.php
@@ -0,0 +1,11 @@
+<?php
+$objCon =mysqli_connect("localhost","root","","");
+// Check connection
+if ($objCon -> connect_errno) {
+echo "Failed to connect to MySQL: " . $objCon ->
+connect_error;
+exit();
+}else
+    echo "Connected to database successfully";
+
+?>
